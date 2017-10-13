7abb3f4d86f81a334d5db25741f408467af5343e
MMXLV <elpaladindx@hotmail.com>
2017-10-12 23:52:23 -0300

    agregada funcion a medias

--- sectest.php
+++ sectest.php
@@ -1,4 +1,12 @@
 <?php
+function adminLevel($dato){
+  //terminar de armar funcion
+  //falta adml en mysql
+  $sql = "SELECT * FROM usuario WHERE LIKE :usuario";
+  $sql->bindValue(":usuario", $dato["usuario"]);
+  $return = fetch_assoc("algo que no recuerdo");
+  $_SESSION["adml"] = $return["adml"];
+}
 if ($_SESSION["adml"] > 10) {
   require_once 'file.php';
 }else(
